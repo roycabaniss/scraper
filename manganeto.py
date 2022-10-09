f595fe656fe6d9a3301af67c92c9deed7622f076
roycabaniss <racaban@gmail.com>
2022-10-09 09:29:11 -0400

    Added worker threads to improve speed

--- manganeto.py
+++ manganeto.py
@@ -7,20 +7,28 @@ import os
 import errno
 import argparse
 from urllib.parse import urljoin
-
-pageNum = 1
+import concurrent.futures
 
 import argparse
 
 p = argparse.ArgumentParser()
 p.add_argument('url', type=str)
+p.add_argument('--pagenum', type=int, default=1)
 args = p.parse_args()
 
 pd = requests.get(args.url, headers={'User-Agent': 'Mozilla/5.0'})
 parse = BeautifulSoup(pd.text, 'html.parser')
 title = parse.find('title').string.strip(' Manga Online Free - Manganato')
 
-nextUrl = parse.find('ul', {'class': 'row-content-chapter'}).find_all('li')[-1].a.get('href')
+nextUrl = parse.find('ul', {'class': 'row-content-chapter'}).find_all('li')[0-args.pagenum].a.get('href')
+
+def fetchImage(url: str):
+    for i in range(5):
+        try:
+            with requests.get(url, headers={'User-Agent': 'Mozilla/5.0', 'referer': 'https://readmanganato.com/'}) as response:
+                return f'<img src=\"data:image/jpg;base64, {base64.b64encode(response.content).decode()}\">\n<br/>\n'
+        except:
+            pass
 
 try:
     os.makedirs(title)
@@ -30,27 +38,18 @@ except OSError as e:
 while nextUrl is not None:
     errCnt = 0
     try:
-        with open(os.path.join(title, f'page{pageNum}.html'), "wb") as dstFile:
+        with open(os.path.join(title, f'page{args.pagenum}.html'), "wb") as dstFile:
             dstFile.write('<html>\n<head><style>\nimg {width: 300px; margin-bottom: -5px;}\n</style></head><body>\n'.encode())
             print(nextUrl)
             with requests.get(nextUrl, headers={'User-Agent': 'Mozilla/5.0'}) as pageDump:
                 parse = BeautifulSoup(pageDump.text, 'html.parser')
                 siw = parse.find('div', {'class':'container-chapter-reader'})
-                for img in siw.findAll('img'):
-                    imgPath = img.get('src')
-                    srcPath = urljoin(nextUrl, imgPath)
-
-                    print('img ', srcPath)
-                    for i in range(5):
-                        try:
-                            with requests.get(srcPath, headers={'User-Agent': 'Mozilla/5.0', 'referer': 'https://readmanganato.com/'}) as response:
-                                dstFile.write('<img src=\"data:image/jpg;base64,'.encode())
-                                dstFile.write(base64.b64encode(response.content))
-                                dstFile.write('\">\n<br/>\n'.encode())
-                        except:
-                            pass
-                        else:
-                            break
+                # We can use a with statement to ensure threads are cleaned up promptly
+                with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
+                    imgTasks = [executor.submit(fetchImage, urljoin(nextUrl, img.get('src'))) for img in siw.find_all('img')]
+                    for task in imgTasks:
+                        dstFile.write(task.result().encode())
+                        
                 next = parse.find(name='a', attrs={'class':'navi-change-chapter-btn-next'})
                 if next:
                     dstFile.write('''
@@ -65,15 +64,15 @@ while nextUrl is not None:
     document.onkeydown = checkKey;
     </script>
     '''.encode())
-                    dstFile.write(f'<a id=\"nextLink\" href=\"page{pageNum+1}.html\">Next</a>\n<br/>\n'.encode())
+                    dstFile.write(f'<a id=\"nextLink\" href=\"page{args.pagenum+1}.html\">Next</a>\n<br/>\n'.encode())
                     nextUrl = urljoin(nextUrl, next.get('href'))
                 else:
                     nextUrl = None
-                if pageNum > 1:
-                    dstFile.write(f'<a id=\"prevLink\" href=\"page{pageNum-1}.html\">Prev</a>\n<br/>\n'.encode())
+                if args.pagenum > 1:
+                    dstFile.write(f'<a id=\"prevLink\" href=\"page{args.pagenum-1}.html\">Prev</a>\n<br/>\n'.encode())
             dstFile.write('</body>\n</html>\n'.encode())
-            pageNum += 1
-            print('New Page', pageNum)
+            args.pagenum += 1
+            print('New Page', args.pagenum)
             time.sleep(2)
     except Exception as err:
         errCnt += 1
