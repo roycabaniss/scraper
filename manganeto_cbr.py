b276a4a91f65f9c0cd86033755302ab141e05210
roycabaniss <racaban@gmail.com>
2023-03-24 01:09:14 -0400

    Switching cbr logic to tasks/executor for chapters

--- manganeto_cbr.py
+++ manganeto_cbr.py
@@ -23,16 +23,14 @@ logger = logging.getLogger(__name__)
 
 p = argparse.ArgumentParser()
 p.add_argument('url', type=str)
-p.add_argument('--pagenum', type=int, default=1)
 p.add_argument('--verbose', '-v', action='count', default=0)
+p.add_argument('--start', type=int, default=0)
 args = p.parse_args()
 
 logger.setLevel(max(logging.WARN - args.verbose * 10, 1))
 pd = requests.get(args.url, headers={'User-Agent': 'Mozilla/5.0'})
 parse = BeautifulSoup(pd.text, 'html.parser')
 title = parse.find('title').string.replace(' Manga Online Free - Manganato', '')
-
-nextUrl = parse.find('ul', {'class': 'row-content-chapter'}).find_all('li')[0-args.pagenum].a.get('href')
 
 def fetchImage(url: str):
     for i in range(5):
@@ -52,35 +50,29 @@ try:
 except OSError as e:
     if e.errno != errno.EEXIST:
         raise
-while nextUrl is not None:
-    errCnt = 0
-    try:
-        with open(os.path.join(title, f'page{args.pagenum:03d}.cbr'), 'wb') as zipdst:
-            with zipfile.ZipFile(zipdst, 'w', zipfile.ZIP_DEFLATED, compresslevel=9) as dstFile:
-                logger.info(nextUrl)
-                with requests.get(nextUrl, headers={'User-Agent': 'Mozilla/5.0'}) as pageDump:
-                    parse = BeautifulSoup(pageDump.text, 'html.parser')
-                    siw = parse.find('div', {'class':'container-chapter-reader'})
-                    # We can use a with statement to ensure threads are cleaned up promptly
-                    with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
-                        imgTasks = [executor.submit(fetchImage, urljoin(nextUrl, img.get('src'))) for img in siw.find_all('img')]
-                        for idx, task in enumerate(imgTasks):
-                            format, content = task.result()
-                            dstFile.writestr(f'{idx:03d}.{format}', content)
-                    next = parse.find(name='a', attrs={'class':'navi-change-chapter-btn-next'})
-                    if next:
-                        nextUrl = urljoin(nextUrl, next.get('href'))
-                    else:
-                        nextUrl = None
-            args.pagenum += 1
-            logger.info(f'New Page {args.pagenum}')
-            time.sleep(2)
-    except Exception as err:
-        errCnt += 1
-        logger.error(err)
-        if errCnt > 5:
-            exit(1)
-        else:
+
+def downloadChapter(nextUrl: str, pagenum: int):
+    for _ in range(5):
+        try:
+            with open(os.path.join(title, f'chapter{pagenum:03d}.cbr'), 'wb') as zipdst:
+                with zipfile.ZipFile(zipdst, 'w', zipfile.ZIP_DEFLATED, compresslevel=9) as dstFile:
+                    logger.info(nextUrl)
+                    with requests.get(nextUrl, headers={'User-Agent': 'Mozilla/5.0'}) as pageDump:
+                        parse = BeautifulSoup(pageDump.text, 'html.parser')
+                        siw = parse.find('div', {'class':'container-chapter-reader'})
+                        # We can use a with statement to ensure threads are cleaned up promptly
+                        with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
+                            imgTasks = [executor.submit(fetchImage, urljoin(nextUrl, img.get('src'))) for img in siw.find_all('img')]
+                            for idx, task in enumerate(imgTasks):
+                                format, content = task.result()
+                                dstFile.writestr(f'{idx:03d}.{format}', content)
+        except Exception as err:
+            logger.error(err)
             logger.error('Retrying')
-    else:
-        errCnt = 0
+        else:
+            return
+
+with concurrent.futures.ThreadPoolExecutor(max_workers=3) as executor:
+    for idx, url in enumerate([x.a.get('href') for x in parse.find('ul', {'class': 'row-content-chapter'}).find_all('li')[::-1][args.start:]], args.start):
+        executor.submit(downloadChapter, url, idx)
+
